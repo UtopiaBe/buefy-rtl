eafeebbd6fa7cb001fda667b9331ad65033ac3a3
Alexander Kravchenko <iqxtech@gmail.com>
2020-08-17 15:17:40 +0300

    master

---
+++ layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+  <div :dir="$dir()">
+    <Header />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      rtl: true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+// Import Bulma's core
+@import "~bulma/sass/utilities/_all";
+
+$rtl: true;
+
+// Import Bulma and Buefy styles
+@import "~bulma";
+@import "~buefy/src/scss/buefy";
+</style>
